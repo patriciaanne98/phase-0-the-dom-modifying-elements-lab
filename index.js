93a64e18c2364de481b47510e57b984b460a6c9d
Patricia Frogoso <pfrogoso@gmail.com>
2023-11-04 13:29:57 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const main = document.getElementById ("main")
+main.remove()
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML = "Patricia is the champion"
